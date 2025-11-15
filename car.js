75d2d35ba964f2c0331b79080ad00d8b58aa2ca0
Krishnanand Yadav <krishnanand2517@gmail.com>
2025-11-16 04:03:04 +0530

    feat: Implement max speed and friction to accelerate and decelerate realistically

--- car.js
+++ car.js
@@ -5,12 +5,17 @@ class Car {
     this.width = width;
     this.height = height;
 
+    this.speed = 0;
+    this.acceleration = 0.2;
+    this.maxSpeed = 3;
+    this.friction = 0.05;
+
     this.controls = new Controls();
   }
 
   update() {
     if (this.controls.forward) {
-      this.y -= 2;
+      this.speed += this.acceleration;
     }
     if (this.controls.left) {
       this.x -= 2;
@@ -19,8 +24,31 @@ class Car {
       this.x += 2;
     }
     if (this.controls.reverse) {
-      this.y += 2;
+      this.speed -= this.acceleration;
     }
+
+    // Cap the max speed
+    if (this.speed > this.maxSpeed) {
+      this.speed = this.maxSpeed;
+    }
+    if (this.speed < -this.maxSpeed / 2) {
+      this.speed = -this.maxSpeed / 2;
+    }
+
+    // Apply friction
+    if (this.speed > 0) {
+      this.speed -= this.friction;
+    }
+    if (this.speed < 0) {
+      this.speed += this.friction;
+    }
+
+    // Stop if speed is lower than friction
+    if (Math.abs(this.speed) < this.friction) {
+      this.speed = 0;
+    }
+
+    this.y -= this.speed;
   }
 
   draw(ctx) {
