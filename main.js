79198523ff42da5a69a26f49437b279e2e561c2c
Krishnanand Yadav <krishnanand2517@gmail.com>
2025-11-16 18:46:13 +0530

    refactor: Handle the borders of the road separately

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ resizeCanvas();
 
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width / 2, canvas.width * 0.9); // leave 10% margin (5% each side)
-const car = new Car(road.getLaneCenter(5), 100, 30, 50);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
