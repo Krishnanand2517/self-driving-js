0c6dfa8f4ec382fb1c4ba12f1c152255f9fa93af
Krishnanand Yadav <krishnanand2517@gmail.com>
2025-11-16 16:41:48 +0530

    feat: Render the road with left and right boundaries

---
+++ road.js
@@ -0,0 +1,29 @@
+class Road {
+  constructor(x, width, laneCount = 3) {
+    this.x = x;
+    this.width = width;
+    this.laneCount = laneCount;
+
+    this.left = x - width / 2;
+    this.right = x + width / 2;
+
+    const INFINITY = 1000000;
+    this.top = -INFINITY;
+    this.bottom = INFINITY;
+  }
+
+  draw(ctx) {
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = "white";
+
+    ctx.beginPath();
+    ctx.moveTo(this.left, this.top);
+    ctx.lineTo(this.left, this.bottom);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(this.right, this.top);
+    ctx.lineTo(this.right, this.bottom);
+    ctx.stroke();
+  }
+}
